6d8cafe47adca312c41693c99ef6023f1c6b6428
manos-per <manosperdikakis@gmail.com>
2020-08-18 16:24:51 +0300

    commit message

--- test_file.R
+++ test_file.R
@@ -1,3 +1,5 @@
 #This is a test file created by Manos
 
 #I want to check if I can push files onto the github repo
+
+#To push this file, I followed the instructions given here: https://cfss.uchicago.edu/setup/git-with-rstudio/
