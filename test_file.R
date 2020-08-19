ecf30eabfa8ddfc9709453ffb2417f76377aa1cc
manos-per <manosperdikakis@gmail.com>
2020-08-19 16:16:15 +0300

    Update: 2020-08-19 16:16:15

--- test_file.R
+++
@@ -1,5 +0,0 @@
-#This is a test file created by Manos
-
-#I want to check if I can push files onto the github repo
-
-#To push this file, I followed the instructions given here: https://cfss.uchicago.edu/setup/git-with-rstudio/
